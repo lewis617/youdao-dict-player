659ff725525a7855b2c0981c1daf04aba0f8bead
lewis617 <897473127@qq.com>
2018-08-04 02:29:09 +0800

    first commit

---
+++ bin/index.js
@@ -0,0 +1,67 @@
+const puppeteer = require('puppeteer');
+const youdaoAccount = process.env.YOUDAO_ACCOUNT.split(';');
+const username = youdaoAccount[0];
+const password = youdaoAccount[1];
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        executablePath: `C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe`,
+        args: [
+            `--window-size=1200,700`
+        ],
+    });
+    // 登录
+    const pages = await browser.pages();
+    const page = pages[0];
+    await page.setViewport({ width: 1200, height: 700 });
+    await page.goto('http://dict.youdao.com/wordbook/wordlist');
+    await page.bringToFront();
+    await page.evaluate((a, b) => {
+        document.querySelector('#username').value = a;
+        document.querySelector('#password').value = b;
+        document.querySelector('.login_btn').click();
+    }, username, password);
+    // 收集所有单词
+    let totalWords = [];
+    let nextPageNum = 2;
+    await page.waitFor('.word');
+    const words = await page.$$eval('.word', els => Array.from(els).map(el => el.textContent));
+    totalWords = [...totalWords, ...words];
+    do {
+        await Promise.all([
+            page.waitForNavigation(),
+            page.$eval('.next-page', el => el.click()),
+        ]);
+        await page.waitFor('.word');
+        const words = await page.$$eval('.word', els => Array.from(els).map(el => el.textContent));
+        totalWords = [...totalWords, ...words];
+        nextPageNum = await page.$$eval('.next-page', els => els.length);
+    } while (nextPageNum === 2);
+    // 遍历每个单词
+    for (let wordIndex = 0; wordIndex < totalWords.length; wordIndex++) {
+        // 听读音
+        const page2 = await browser.newPage();
+        await page2.setViewport({ width: 1200, height: 700 });
+        await page2.goto(`http://dict.youdao.com/search?q=${totalWords[wordIndex]}`);
+        await page2.$$eval('.dictvoice', els => {
+            els[0].click();
+        });
+        await page2.waitFor(3000);
+
+        // 看图片
+        const page3 = await browser.newPage();
+        await page3.setViewport({ width: 1200, height: 700 });
+        await page3.goto(`https://cn.bing.com/images/search?q=${totalWords[wordIndex]}&ensearch=1`);
+        for (let i = 0; i < 10; i++) {
+            await page3.keyboard.press('ArrowDown');
+            await page3.waitFor(500);
+        }
+
+        await page2.close();
+        await page3.close();
+    }
+
+    await browser.close();
+})();
+
